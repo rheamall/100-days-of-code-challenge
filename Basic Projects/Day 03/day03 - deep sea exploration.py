597889c7f50533807458d571b22d27ba08111693
Rhea Mall <165444162+rheamall@users.noreply.github.com>
2024-07-03 03:25:24 +0100

    Add files via upload

--- Basic Projects/Day 03/day03 - deep sea exploration.py
+++ Basic Projects/Day 03/day03 - deep sea exploration.py
@@ -16,34 +16,24 @@ logo = '''
 '''
 
 print(logo)
-print(
-    "Your mission is to find the lost treasure from a ship that sank 200 years ago."
-)
+print("Your mission is to find the lost treasure from a ship that sank 200 years ago.") 
 
-direc = input(
-    '\nYou\'re on a boat in the middle of the ocean. Do you want to "dive" or "stay" on the boat? \n'
-)
+direc = input('You\'re on a boat in the middle of the ocean. Do you want to "dive" or "stay" on the boat? \n')
 if direc.lower() == 'stay':
-  print('There was a leak in the boat - it sinks. Game over!')
+    print('There was a leak in the boat - it sinks. Game over!')
 else:
-  cave = input(
-      '\nYou\'ve descended into the depths. You see a cave ahead. Type "enter" to explore the cave. Type "swim" to continue\nswimming forward. \n'
-  )
-  if cave.lower() == 'swim':
-    print('You get attacked by a great white shark. Game Over!')
-  else:
-    paths = input(
-        '\nYou enter the cave and see two paths. One is narrow and dark, the other is wide and well-lit. Do you choose the\n"narrow" path or the "wide" path? \n'
-    )
-    if paths.lower() == 'narrow':
-      print('You get lost in a dark maze. Game Over!')
+    cave = input('You\'ve descended into the depths. You see a cave ahead. Type "enter" to explore the cave. Type "swim" to continue swimming forward. \n')
+    if cave.lower() == 'swim':
+        print('You get attacked by a great white shark. Game Over!')
     else:
-      treasure = input(
-          '\nYou navigate through the wide path and discover three underwater temples: one made of coral, one of crystal and\none of pearls. Which temple do you explore? Type "coral", "crystal" or "pearls". \n'
-      )
-      if treasure.lower() == 'coral':
-        print('The coral temple is filled with sea urchins. Game Over!')
-      elif treasure.lower() == 'crystal':
-        print('The crystal temple collapses around you. Game Over!')
-      elif treasure.lower() == 'pearls':
-        print('You\'ve found the lost treasure - mission successful! You Win!')
+        paths = input('You enter the cave and see two paths. One is narrow and dark, the other is wide and well-lit. Do you choose the "narrow" path or the "wide" path? \n')
+        if paths.lower() == 'narrow':
+            print('You get lost in a dark maze. Game Over!')
+        else:
+            treasure = input('You navigate through the wide path and discover three underwater temples: one made of coral, one of crystal and one of pearls. Which temple do you explore? Type "coral", "crystal" or "pearls". \n')
+            if treasure.lower() == 'coral':
+                print('The coral temple is filled with sea urchins. Game Over!')
+            elif treasure.lower() == 'crystal':
+                print('The crystal temple collapses around you. Game Over!')
+            elif treasure.lower() == 'pearls':
+                print('You\'ve found the lost treasure - mission successful! You Win!')
